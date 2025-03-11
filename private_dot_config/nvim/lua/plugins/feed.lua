ce17a03a212ea0791084e95e04468523ad21a911
illegitimate-egg <cameron@humaneyestudio.co.uk>
2025-03-11 00:35:07 +0000

    Add feed support

---
+++ private_dot_config/nvim/lua/plugins/feed.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "neo451/feed.nvim",
+    cmd = "Feed",
+    opts = {
+      feeds = {
+        "https://github.com/illegitimate-egg/mcrizzledizzle/releases.atom",
+        "https://github.com/illegitimate-egg/mcrizzledizzle/commits.atom",
+        "https://archlinux.org/feeds/news/",
+        "https://archlinux.org/feeds/packages/",
+        "https://archlinux.org/feeds/packages/added/",
+        "https://archlinux.org/feeds/packages/removed/",
+      },
+    },
+  },
+}
