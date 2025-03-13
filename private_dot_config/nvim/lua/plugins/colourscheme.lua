a3b8dc1fb2ed80ec5d6b5509b8f16c4039110f78
illegitimate-egg <cameron@humaneyestudio.co.uk>
2025-03-13 20:50:24 +0000

    Change colourscheme

--- private_dot_config/nvim/lua/plugins/colourscheme.lua
+++ private_dot_config/nvim/lua/plugins/colourscheme.lua
@@ -1,20 +1,27 @@
 return {
-  { "sjl/badwolf" },
+  -- { "sjl/badwolf" },
+  {
+    "briones-gabriel/darcula-solid.nvim",
+    dependencies = {
+      "rktjmp/lush.nvim",
+    },
+  },
 
   -- Load that hot beast
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "badwolf",
+      -- colorscheme = "badwolf",
+      colorscheme = "darcula-solid",
     },
   },
 
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    opts = function(_, opts)
-      opts.options.theme = "gruvbox"
-      return opts
-    end,
-  },
+  -- {
+  --   "nvim-lualine/lualine.nvim",
+  --   event = "VeryLazy",
+  --   opts = function(_, opts)
+  --     opts.options.theme = "gruvbox"
+  --     return opts
+  --   end,
+  -- },
 }
